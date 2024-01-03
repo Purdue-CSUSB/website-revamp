b7fbb1af18172c7dcd56b0f9e2fd64df875c582d
Shruti <sharmas0104@gmail.com>
2024-01-03 00:27:29 -0600

    navboar dropdown for initiatives

--- src/Navbar.js
+++ src/Navbar.js
@@ -17,20 +17,35 @@ export default function Navbar() {
         <div className="links">
         <Button className="hamburger">M</Button>
         <ul>
-            <li>
-            <Link to = "/"> Home </Link>
-            </li>
+
             <li>
             <Link to = "/addBlog"> Add Stuff </Link>
             </li>
             <li>
-            <Link to = "/displayBlog"> See Blogs </Link>
+            <Link to = "/displayBlog"> Humans of Purdue </Link>
             </li>
             <li>
             <Link to = "/wiki"> Student Wiki </Link>
             </li>
+
+            <div class="dropdown">
+                <button class="dropbtn"> Initiatives </button>
+                <div class="dropdown-content">
+                <li>
+                    <Link to = "/clubhub"> Club Hub </Link>
+                </li>
+                <li>
+                    <Link to = "/ptp"> Purdue Technical Projects </Link>
+                </li>
+
+                   
+                </div>
+             </div>
             <li>
-            <Link to = "/initiatives"> Initiatives </Link>
+                <Link to = "/contact"> Contact Us </Link>
+            </li>
+            <li>
+                <Link to = "/about"> About </Link>
             </li>
         </ul>
         </div>

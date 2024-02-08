8aea37495aac47e85c01f070fd6cc62669f28024
Shruti <sharmas0104@gmail.com>
2024-02-08 01:53:58 -0500

    created social media carousel
    - all images for instagram posts are stored in the public folder
    - might still want to try and find a way to update the dot color at the bottom

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,7 @@
 // General Imports
 import { Link, useNavigate } from "react-router-dom";
-import * as React from 'react';
+//import * as React from 'react';
+import React, { useState } from 'react';
 import './Landing.css';
 
 // Importing MUI Components
@@ -15,13 +16,36 @@ import MenuBookIcon from '@mui/icons-material/MenuBook';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Menu } from "@mui/base";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { InstagramEmbed } from 'react-social-media-embed'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from "@mui/material/CardMedia";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+
+
+
+const postData = [ {src: "/banquet.jpg", link:'https://www.instagram.com/p/C0cXh4fJuAf/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' },
+                {src:"/helproom.jpg", link:"https://www.instagram.com/p/C2nmK1Qpsc4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+                {src:"/CoC.jpg", link:"https://www.instagram.com/p/C2AUKfNL-Oy/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="}, 
+                {src:"/innovateher.jpg", link:"https://www.instagram.com/p/C0FZzBLJ4Ll/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+                {src:"/2024callout.jpg", link:"https://www.instagram.com/p/C15aRw2JiwP/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="}
+                ];
+
+
+
+
 
 const theme = createTheme({
-  palette: {
-    yellow: {
-      main: '#ffc700'
+    palette: {
+        yellow: {
+            main: '#ffc700'
+        },
     },
-  },
 });
 
 // Defined constants
@@ -31,7 +55,66 @@ const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-  }));
+}));
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    
+  };
+
+
+  function SocialMediaCard({src, link})  {
+
+    const [heartColor, setHeartColor] = useState('gray');
+    const [shareColor, setShareColor] = useState('gray');
+
+
+    const handleFavorite= () => {
+        if (heartColor === 'gray')
+            setHeartColor('red');
+        else{
+            setHeartColor('gray')
+        }
+    };
+
+    const handleShare= () => {
+        if (shareColor === 'gray')
+            setShareColor('red');
+        else{
+            setShareColor('gray')
+        }
+        
+        window.open(link);
+      };
+
+      return (
+
+            <Card sx={{ height: 400, width: 375}}>
+            <img 
+                src={src} 
+                style={{ 
+                    height: '300px', 
+                    padding: '25px',
+                    borderRadius: '30px'
+                }}
+            />
+            <IconButton onClick={handleFavorite} sx={{marginLeft:'25px', marginTop: -2}}>
+                <FavoriteIcon style={{ color: heartColor }} />
+            </IconButton>
+            <IconButton onClick={handleShare} sx={{ marginTop: -2}}>
+                <ShareOutlinedIcon style={{ color: shareColor }} />
+            </IconButton>   
+        </Card>
+
+        );
+
+
+  }
 
 
 
@@ -41,71 +124,51 @@ export default function Landing() {
 
     const handleClick = () => {
         navigate('/wiki');
-      };
-  return (
-    // Container for Hello from USB
-    // FIXME: Convert this to a box potentially, add padding.
-    /* 
-    <Container class="Hello" maxWidth="sm">
-        <Grid container spacing={2}>
-            <Grid item xs={5}>
-                <h1>Hello from USB</h1>
-                <p>The Computer Science Undergraduate Board is dedicated to improving the student experience 
-                    within Computer and Data Sciences at Purdue - whether by managing CS193, hosting forums for student advocacy, 
-                    or maintaining a database of undergrad-tailored resources, USB is continually creating initiatives 
-                    that will uphold our values.</p>
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Link to = "/initiatives">
-                            <Button hover variant="contained">Our Initiatives</Button>
-                        </Link>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Link to = "/wiki">
-                            <Button hover variant="contained">Read Student Wiki</Button>
-                        </Link>
-                    </Grid>
-                </Grid>
+    };
 
-            </Grid>
-            <Grid item xs={6}>
-                <img src = {require('./images/team2022.png')}/> 
-             </Grid>
-        </Grid>
 
-    
-    </Container>
+    return (
+        <div>
+            <div className="welcome">
+                <div className="intro">
+                    <h1>Hello from USB! </h1>
+                    <h2> Strengthening the Purdue Computing Student Experience since 1999 </h2>
+                    <ThemeProvider theme={theme}>
+                        <Stack direction="row" spacing={2} justifyContent="center" marginTop={-6}>
+                            <Button color="yellow" size="large" variant="outlined" startIcon={<MenuBookIcon />}
+                                onClick={handleClick}>
+                                Read Student Wiki
+                            </Button>
+                            <Button color="yellow" size="large" variant="outlined" startIcon={<TipsAndUpdatesIcon />}>
+                                Explore Initiatives
+                            </Button>
+                        </Stack>
+                    </ThemeProvider>
+                </div>
+                <div className="photo">
 
-    */
+                    <img src={require('./images/team2022.png')} />
 
-    <div className = "welcome">
-        <div className = "intro">
-            <h1>Hello from USB! </h1>
-            <h2> Strengthening the Purdue Computing Student Experience since 1999 </h2>
-            <ThemeProvider theme={theme}>
-                <Stack direction="row" spacing={2} justifyContent = "center" marginTop={-6}>
-                    <Button color = "yellow" size = "large" variant="outlined" startIcon={<MenuBookIcon />} 
-                    onClick={handleClick}>
-                        Read Student Wiki
-                    </Button>
-                    <Button color = "yellow" size = "large" variant="outlined" startIcon={<TipsAndUpdatesIcon />}>
-                        Explore Initiatives
-                    </Button>
-                </Stack>
-            </ThemeProvider>
-        </div>
-        <div className = "photo">
+                </div>
 
-            <img src = {require('./images/team2022.png')}/> 
 
+            </div>
+
+            <div className = "social">
+                <h1 class = "h1Social"> Stay Connected</h1>
+
+                <Slider {...settings}>
+                    {postData.map((post) => (
+                    <SocialMediaCard src={post.src} link={post.link} />
+                    ))}
+                </Slider> 
+
+            </div>
+
+            
         </div>
 
-
-    </div>
-
-
-    // TODO: Container for Stay Connected
-    // TODO: Container for Meet the Board
-    // TODO: Container for About Us
-  );
+        // TODO: Container for Meet the Board
+        // TODO: Container for About Us
+    );
 }

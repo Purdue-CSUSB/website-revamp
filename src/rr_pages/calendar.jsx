e33cc060206bc120f884cb455f78ddff86fd6eea
cjoslin <cjoslin@purdue.edu>
2024-07-07 16:58:18 -0400

    Embedding of Google Calendar Complete (Note* the calendar is visible ONLY on the Google Chrome browser)

--- src/rr_pages/calendar.jsx
+++ src/rr_pages/calendar.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
 function calendar() {
-
-
-
+    const calenderId = '88e0056451d336ea63dbb2cfc2ae5691fb511e40dbcc5e5d79dc6ed439fb7337@group.calendar.google.com'
+    const timeZone = 'America%2FIndiana%2FIndianapolis'
     return (
-
-        <div>  
-            <h1> Calendar </h1>
-        </div>
-
+        <iframe
+        title="Google Calendar" 
+        src={`https://calendar.google.com/calendar/embed?src=${calenderId}&ctz=${timeZone}`}
+        style={{ border: '0', width: '800px', height: '600px', frameborder: '0', scrolling: 'no' }}
+        allowFullScreen
+        ></iframe>
     );
+  }
 
-
-}
-export default calendar;
+  const MyComponent = () => {
+    return (
+        <div>
+            <h1> Calendar </h1>
+            {calendar()}
+        </div>
+    );
+  };
+  
+export default MyComponent; 

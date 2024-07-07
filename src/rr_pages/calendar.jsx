2605c12d29e9ba5f52fd6be37ffae200f2b444e0
cjoslin <cjoslin@purdue.edu>
2024-07-07 17:46:11 -0400

    RR updates to embedded calendar works with other browsers besides Chrome

--- src/rr_pages/calendar.jsx
+++ src/rr_pages/calendar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function calendar() {
     const calenderId = '88e0056451d336ea63dbb2cfc2ae5691fb511e40dbcc5e5d79dc6ed439fb7337@group.calendar.google.com'
     const timeZone = 'America%2FIndiana%2FIndianapolis'
-    return (
+    return ( 
         <iframe
         title="Google Calendar" 
         src={`https://calendar.google.com/calendar/embed?src=${calenderId}&ctz=${timeZone}`}

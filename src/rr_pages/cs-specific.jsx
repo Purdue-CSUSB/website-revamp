721445c5c53ebf5eda418fbaac337fe258b5e37e
cjoslin <cjoslin@purdue.edu>
2024-09-05 18:06:35 -0400

    Styling for CS-Specific RR page complete

--- src/rr_pages/cs-specific.jsx
+++ src/rr_pages/cs-specific.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Stack } from "@mui/material";
 import StickySideNav from "../StickySideNav";
 import "../Research.css"
+import './cs-specific.css';
 
 function csSpecific() {
 
@@ -19,56 +20,50 @@ function csSpecific() {
                 </div>
 
 
-                <div className='main content'>
+            <div className='research-with-prof' >
+
+                <h3>
+                    Undergraduate Research Opportunities with CS Professor
+                        <a href="https://www.cs.purdue.edu/corporate/employment/cs%20research.html"> (Click here)
+                            </a>
+                </h3>
+
+                <div className='text'>
+                    <p> <strong> Description:</strong> An updated list of research projects with CS professors who are seeking undergraduate researchers.</p>
+                </div>
+
+                <div className='text'>
+                     <p> <strong> How it can help:</strong>  If you already have an interest in a specific research area  or would like to work with a certain professor on a project.</p>
+                </div>
+
+            </div>
 
 
-                    <div className='Research with CS Professor ' >
+            <div className="cs-departmental-research-areas" >
 
-                        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                            <u> Undergraduate Research Opportunities with CS Professor
-                                <a href="https://www.cs.purdue.edu/corporate/employment/cs%20research.html"> (Click here)
-                                </a>
-                            </u>
-                        </h3>
+                <h3>
+                    CS Departmental Research Areas
+                    <a href="https://www.cs.purdue.edu/research/index.html"> (Click here)
+                        </a>
+                </h3>
 
-                        <div className='text'>
-                            <p> <strong> Description:</strong> An updated list of research projects with CS professors who are seeking undergraduate researchers.</p>
-                        </div>
-
-                        <div className='text'>
-                            <p> <strong> How it can help:</strong>  If you already have an interest in a specific research area  or would like to work with a certain professor on a project.</p>
-                        </div>
-
+                    <div className='text'>
+                        <p> <strong> Description:</strong> A summary of the 14 major research areas within the CS Department and the supporting faculty for each.</p>
                     </div>
 
-
-                    <div className="CS Departmental Research Areas" >
-
-                        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                            <u> CS Departmental Research Areas
-                                <a href="https://www.cs.purdue.edu/research/index.html"> (Click here)
-                                </a>
-                            </u>
-                        </h3>
-
-                        <div className='text'>
-                            <p> <strong> Description:</strong> A summary of the 14 major research areas within the CS Department and the supporting faculty for each.</p>
-                        </div>
-
-                        <div className='text'>
-                            <p> <strong> How it can help:</strong>  If you do not have a specific research area or professor in mind yet but want to explore all the options available in the Department.</p>
-                        </div>
-
+                    <div className='text'>
+                        <p> <strong> How it can help:</strong>  If you do not have a specific research area or professor in mind yet but want to explore all the options available in the Department.</p>
                     </div>
 
+            </div>
 
-                    <div className="Research Seminars" >
 
-                        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                            <u> Research Seminars
-                                <a href="https://www.cs.purdue.edu/research/seminars.html"> (Click here)
-                                </a>
-                            </u>
+            <div className="research-seminars" >
+
+                <h3>
+                        Research Seminars
+                        <a href="https://www.cs.purdue.edu/research/seminars.html"> (Click here)
+                            </a>
                         </h3>
 
                         <div className='text'>
@@ -82,14 +77,13 @@ function csSpecific() {
                     </div>
 
 
-                    <div className="BrightSpace Page">
+            <div className="brightspace-page">
 
-                        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-                            <u> Computational Science Undergrad Research Brightspace Page
-                                <a href="https://purdue.brightspace.com/d2l/login?sessionExpired=1&target=%2fd2l%2fle%2fcontent%2f910376%2fviewContent%2f13784501%2fView"> (Click here)
+                <h3>
+                        Computational Science Undergrad Research Brightspace Page
+                            <a href="https://purdue.brightspace.com/d2l/login?sessionExpired=1&target=%2fd2l%2fle%2fcontent%2f910376%2fviewContent%2f13784501%2fView"> (Click here)
                                 </a>
-                            </u>
-                        </h3>
+                    </h3>
 
                         <div className='text'>
                             <p> <strong> Description:</strong> A list of all the undergraduate research programs that offer CS-related projects.</p>
@@ -101,19 +95,25 @@ function csSpecific() {
                     </div>
 
 
-                </div>
+               
 
 
+            <div className='questions'>
+                <h3> 
+                    Need More Help? Schedule an Appointment!
+                </h3>
                 <div className='text'>
                     <p> Still have some questions that were not answered on this page or in our FAQ Section?
                         <a href="https://calendly.com/csstudentaffairs"> Schedule an Appointment with Amber Stanley
-                        </a>
-                    </p>
+                            </a>
+                        </p>
                 </div>
-
+                
             </div>
+
+         </div>
             </Stack>
-        </div>
+    </div>
     );
 }
 export default csSpecific;
